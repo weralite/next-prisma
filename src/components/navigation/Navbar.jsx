5f11c4cd019462ce952251d860b21a253c10d74d
mazmann <sadi.lundberg@hotmail.com>
2024-09-18 11:45:37 +0200

    implemented usepathname

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,23 +1,31 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Navbar() {
-const pathName = usePathname();
+  const pathName = usePathname();
+
+  const siteRoutes = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Contact", path: "/contact" },
+  ];
 
   return (
     <nav>
-   <ul className="flex flex-row gap-5">
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/about">About</a>
-        </li>
-        <li>
-            <a href="/contact">Contact</a>
-        </li>
-   </ul>
+      <ul className="flex flex-row gap-5">
+        {siteRoutes.map((siteRoute) => (
+          <li
+            key={siteRoute.path}
+            className={`relative ${pathName === siteRoute.path ? "after:content-[''] after:absolute after:w-full after:h-[2px] after:bg-current after:left-0 after:bottom-0 after:transition-all after:duration-150" : "after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-current after:left-1/2 after:bottom-0 after:transition-all after:duration-150 hover:after:w-full hover:after:left-0"}`}
+          >
+            <Link href={siteRoute.path}>{siteRoute.name}</Link>
+          </li>
+        )
+
+        )}
+      </ul>
     </nav>
   );
 }

913f1009319fc58321cfc5a7c66f4bde6fb85431
mazmann <sadi.lundberg@hotmail.com>
2024-09-18 15:38:26 +0200

    added slugmiddleware

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,39 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
+// Funktion för att skapa en slug från en titel
+const createSlug = (title) => title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
+
+// PrismaClient singleton funktion
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  // Skapa en Prisma Client med extension
+  return new PrismaClient().$extends({
+    query: {
+      post: {
+        async create({ args, query }) {
+          // Automatisk slug-skapning vid post-creation
+          if (args.data.title) {
+            args.data.slug = createSlug(args.data.title);
+          }
+          return query(args);
+        },
+        async update({ args, query }) {
+          // Automatisk slug-skapning vid post-update
+          if (args.data.title) {
+            args.data.slug = createSlug(args.data.title);
+          }
+          return query(args);
+        },
+      },
+    },
+  });
+};
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
